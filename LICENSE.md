7d6b31f0cb6f66e1c514422c02de1afa4f9517d9
Artem Ploujnikov <flexthink@users.noreply.github.com>
2021-07-21 00:06:21 -0400

    Initial import

---
+++ LICENSE.md
@@ -0,0 +1,20 @@
+# License
+## Custom Code
+Any custom code and manifests for the dataset is provided under the MIT license:
+
+Copyright 2021 Artem Ploujnikov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Datasets
+
+The data is provided under the respective licenses from which the dataset was derived.
+### LibriSpeech
+LibriSpeech: an ASR corpus based on public domain audio books", Vassil Panayotov, Guoguo Chen, Daniel Povey and Sanjeev Khudanpur, ICASSP 2015
+
+Provided under the [Creative Commons License v4.0](https://creativecommons.org/licenses/by/4.0/)
+
---
+++ librig2p-nostress-space.py
@@ -0,0 +1,120 @@
+# coding=utf-8
+# Copyright 2021 Artem Ploujnikov
+
+
+# Lint as: python3
+import json
+
+import datasets
+
+_DESCRIPTION = """\
+Grapheme-to-Phoneme training, validation and test sets
+"""
+
+_BASE_URL = "https://raw.githubusercontent.com/flexthink/librig2p-nostress-space/develop/dataset"
+
+_HOMEPAGE_URL = "https://github.com/flexthink/librig2p-nostress-space/tree/develop"
+
+_PHONEMES = [
+    "AA",
+    "AE",
+    "AH",
+    "AO",
+    "AW",
+    "AY",
+    "B",
+    "CH",
+    "D",
+    "DH",
+    "EH",
+    "ER",
+    "EY",
+    "F",
+    "G",
+    "HH",
+    "IH",
+    "IY",
+    "JH",
+    "K",
+    "L",
+    "M",
+    "N",
+    "NG",
+    "OW",
+    "OY",
+    "P",
+    "R",
+    "S",
+    "SH",
+    "T",
+    "TH",
+    "UH",
+    "UW",
+    "V",
+    "W",
+    "Y",
+    "Z",
+    "ZH",
+    " "
+]
+_ORIGINS = ["librispeech", "librispeech-lex"]
+_NA = "N/A"
+_SPLIT_TYPES = ["train", "valid", "test"]
+_DATA_TYPES = ["lexicon", "sentence"]
+_SPLITS = [
+    f"{data_type}_{split_type}"
+    for data_type in _DATA_TYPES
+    for split_type in _SPLIT_TYPES]
+
+class GraphemeToPhoneme(datasets.GeneratorBasedBuilder):
+    def __init__(self, base_url=None, splits=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.base_url = base_url or _BASE_URL
+        self.splits = splits or _SPLITS
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "speaker_id": datasets.Value("string"),
+                    "origin": datasets.ClassLabel(names=_ORIGINS),
+                    "char": datasets.Value("string"),
+                    "phn": datasets.Sequence(datasets.ClassLabel(names=_PHONEMES)),
+                },
+            ),
+            supervised_keys=None,
+            homepage=_HOMEPAGE_URL,
+        )
+
+    def _get_url(self, split):
+        return f'{self.base_url}/{split}.json'
+
+    def _split_generator(self, dl_manager, split):
+        url = self._get_url(split)
+        path = dl_manager.download_and_extract(url)
+        return datasets.SplitGenerator(
+            name=split,
+            gen_kwargs={"datapath": path, "datatype": split},
+        )
+
+    def _split_generators(self, dl_manager):
+        return [
+            self._split_generator(dl_manager, split)
+            for split in self.splits
+        ]
+
+    def _generate_examples(self, datapath, datatype):
+        with open(datapath, encoding="utf-8") as f:
+            data = json.load(f)
+
+        for sentence_counter, (item_id, item) in enumerate(data.items()):
+            resp = {
+                "id": item_id,
+                "speaker_id": str(item.get("speaker_id") or _NA),
+                "origin": item["origin"],
+                "char": item["char"],
+                "phn": item["phn"],
+            }
+            yield sentence_counter, resp
